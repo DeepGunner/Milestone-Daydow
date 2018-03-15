3d69b02af0c1c0ec2e9df887d20cc1505c3cdd56
akshayreddy <akshayreddychinnolla@gmail.com>
2018-03-15 12:32:26 +0530

    main.js file is add

---
+++ client/src/Main.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Main = () =>
+<div>
+<h1> ekart </h1>
+	<input type="text" className="in" onChange={this.getName} placeholder="name"/>
+	</div> )
+
+
+export default Main
