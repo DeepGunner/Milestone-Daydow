4e18d11be1cf88a11ff96f52c05a5195b51ba439
deepgunner <deepindersingh.23@gmail.com>
2018-03-15 18:22:40 +0530

    successful integration of FE and BE for the frontpage

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var bodyParser = require('body-parser');
 var Comment = require('./model/comments');
 
 //To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
@@ -35,41 +35,41 @@ app.use(function(req, res, next) {
 });
 
 //now  we can set the route path & initialize the API
-router.get('/', function(req, res) {
-    res.json({ message: 'API Initialized!'});
-  });
-  
-  //adding the /comments route to our /api router
-  router.route('/comments')
-    //retrieve all comments from the database
-    .get(function(req, res) {
-      //looks at our Comment Schema
-      Comment.find(function(err, comments) {
-        if (err)
-          res.send(err);
-        //responds with a json object of our database comments.
-        res.json(comments)
-      });
-    })
-    //post new comment to the database
-    // .post(function(req, res) {
-    //   var comment = new Comment();
-    //   //body parser lets us use the req.body
-    //   comment.author = req.body.author;
-    //   comment.text = req.body.text;
-  
-    //   comment.save(function(err) {
-    //     if (err)
-    //       res.send(err);
-    //     res.json({ message: 'Comment successfully added!' });
-    //   });
+router.get('/', function (req, res) {
+  res.json({ message: 'API Initialized!' });
+});
+
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(async function (req, res) {
+    // /api/comments?page=1&count=20
+    var products;
+
+    var page = Math.max(parseInt(req.query.page) - 1, 0) || 0;
+    var count = Math.max(parseInt(req.query.count), 10) || 20;
+    // The default limit is assumed at 20 entries per page
+    //looks at our Comment Schema
+    products = await Comment.find({})
+        .skip(parseInt(page) * parseInt(count))
+        .limit(parseInt(count))
+    
+    return res.json(products);
+
+    // Comment.find(function (err, comments) {
+    //   if (err)
+    //     res.send(err);
+    //   //responds with a json object of our database comments.
+    //   res.json(comments)
     // });
+  })
+
 
 //Use our router configuration when we call /api
 app.use('/api', router);
 
 //starts the server and listens for requests
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`api running on port ${port}`);
 });
 
