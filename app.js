2f4319af1f1c734c95725d89e4bdfa9a469f054c
Ben Sooraj <bensoorajm@gmail.com>
2018-03-15 16:12:53 +0530

    na

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(session({
     resave: false,
     cookie: {
         httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 1 * 1,
+        maxAge: 1000 * 60 * 60 * 1 * 1, // 1000 * 60 * 60 * 24 * 7 
     },
     saveUninitialized: true,
     store: cartSessionStore,
