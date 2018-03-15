c5e684a36ac8805019785656057cbf65ffd78a30
Ben Sooraj <bensoorajm@gmail.com>
2018-03-15 12:00:38 +0530

    return individual product by 'sku' for example 'BJu1j9LFM'

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// All products
 router.get('/product', async function (req, res) {
     var products = await db.Product.find({}).limit(20);
     return res.json(products);
 })
 
-// router.get('/product/:id', function (req, res) {
-    
-// })
+// For a single product
+router.get('/product/:sku', async function (req, res) {
+    var product = await db.Product.find({
+        sku: req.params.sku
+    })
+    return res.json(product);
+})
 
 module.exports = router;
