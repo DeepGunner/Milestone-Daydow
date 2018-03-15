59f2e7a9f2035bad60d97565040666c92bdeb5fe
Ben Sooraj <bensoorajm@gmail.com>
2018-03-15 13:32:52 +0530

    pagination support added, for '/api/product'

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+// Helper function
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
 // All products
 router.get('/product', async function (req, res) {
-    var products = await db.Product.find({}).limit(20);
+    // This route doesn't handle errors well, when
+    // page or count values are enormously high or negative
+    // Need to add Unhandled Promise rejections for the async/await combo
+    var products;
+
+    var page = Math.max(parseInt(req.query.page) - 1, 0) || 0;
+    var count = Math.max(parseInt(req.query.count), 10) || 20;
+    // The default limit is assumed at 20 entries per page
+    products = await db.Product.find({})
+        .skip(parseInt(page) * parseInt(count))
+        .limit(parseInt(count))
     return res.json(products);
 })
 
---
+++ utilities/errorResponse.js
@@ -0,0 +1,7 @@
+module.exports = {
+    quitPlaying: {
+        error: true,
+        artist: "Backstreet Boys",
+        lyrics: "Quit playing games with my heart (with my heart), \nBefore you tear us apart (my heart), \nQuit playing games with my heart, \nI should have known from the start, \nYou know you have got to stop (from my heart), \nYou are tearing us apart (my heart, my heart), \nQuit playin' games with my heart",
+    }
+}
